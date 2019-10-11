639cc44c016e44318eb4f0e78ac89ab47a624836
wuchunlong0 <wcl6005@163.com>
2019-10-11 16:15:06 +0800

    混合排序

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -137,5 +137,5 @@ STATICFILES_DIRS = (
 )
 
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/schedule/list/'
+LOGOUT_REDIRECT_URL = '/schedule/list/'
